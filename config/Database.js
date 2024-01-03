902e5653742c1dff6b9bcccea0c36baf91e301f4
arilfajri <arielmunchen60@gmail.com>
2024-01-03 18:37:42 +0700

    test deploy

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db_groovefootball", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
+const db = new Sequelize({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default db;
