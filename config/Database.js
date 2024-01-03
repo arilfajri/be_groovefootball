b2fb2be442ca7457045b5a4f1b5293c01e1c15b0
arilfajri <arielmunchen60@gmail.com>
2024-01-03 19:11:39 +0700

    fix3

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const db = new Sequelize(
+  process.env.DB_DBNAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 export default db;
