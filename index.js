58f61c83cd6b7b317e49073b95dd9854a16ed9a8
arilfajri <arielmunchen60@gmail.com>
2024-01-04 13:17:46 +0700

    fix lagi3

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-);
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://fe-groovefootball.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
