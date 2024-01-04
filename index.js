80f3a3f3fd5872d8c822dd53ebfdb606f6598e96
arilfajri <arielmunchen60@gmail.com>
2024-01-04 13:01:01 +0700

    test2

--- index.js
+++ index.js
@@ -16,21 +16,9 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://fe-groovefootball.vercel.app"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.header("Access-Control-Allow-Credentials", "true"); // Setel ke 'true'
-//   next();
-// });
-
 app.use(
-  cors({ credentials: true, origin: "https://fe-groovefootball.vercel.app" })
+  cors({ credentials: true, origin: "https://fe-groovefootball.vercel.app/" })
 );
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(FileUpload());
